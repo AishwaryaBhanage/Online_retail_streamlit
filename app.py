ad8653e2dbd2c6053a96aec8afa308ad5bbe501f
AishwaryaBhanage <aish.bhanage0412@gmail.com>
2023-07-04 12:51:59 +0530

    streamlit app visualization

--- app.py
+++ app.py
@@ -19,7 +19,28 @@ products = pd.read_csv(products_data)
 selected_option = st.selectbox("Select product to view recommendations:",products)
 st.write("You selected: ", selected_option)
 
-print(selected_option)
-#plot a bar chart
+rules = pd.read_csv("Notebooks/datasets/rules.csv")
+df = pd.DataFrame(rules)
+ 
 
+var = df[df['antecedents']==selected_option]
 
+var2 = var.sort_values(by = "support", ascending=False)
+
+toprecommendations =  var2.head(5)
+
+#error - when you try to compare a pandas column to a list but they have different lengths
+# plotting of bar chart
+bar_chart = px.bar(toprecommendations, x='support', y='consequents', orientation='h')
+bar_chart.update_layout(yaxis=dict(autorange="reversed"))
+
+st.plotly_chart(bar_chart)
+ 
+# creating shopping lis with top recommendations
+selected_option_2 = st.selectbox("Select recommendation to add to list:",toprecommendations["consequents"])
+st.write("You selected: ", selected_option_2)
+
+shopping_list = []
+shopping_list.append(selected_option_2)
+
+st.write(shopping_list)
